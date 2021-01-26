00f3c9adf5c6a8ef41f8bc26c3895eb9fde6b36b
gbernat <guido.bernat@gmail.com>
2021-01-26 12:41:40 -0300

    Deploy with Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,13 +4,9 @@ variable "forensics_S3_bucket_name" {
 }
 
 variable "ec2_key" {
-    # Remove me!
-    default = "/Users/guido/Documents/AWSCourse/EC2tutorial.pem"
     description = "Path to key.pem to access by ssh to vulnerated EC2" 
 }
 
 variable vpc_id {
-    # Remove me!
-    default = "vpc-3c47a55a"
     description = "VPC id for the creation of the Security Group"
 }
